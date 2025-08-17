da6a908fee2bf2151b67981beb5e672241e1e04a
Pistiaan <christiaan.timmer.97@gmail.com>
2025-08-17 10:18:53 +0200

    CH3 L1

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-from stats import count_words, count_letters
+from stats import count_words, count_letters, book_report
 
 def get_book_text(filepath):
     with open(filepath) as f:
         content = f.read()
     return content
 
+def print_report(filepath, content_string, dict_list):
+    print('============ BOOKBOT ============')
+    print(f'Analyzing book found at {filepath}...')
+    print('----------- Word Count ----------')
+    count_words(content_string)
+    print('--------- Character Count -------')
+    for entry in dict_list:
+        print(f'{entry["char"]}: {entry["num"]}')
+    print('============= END ===============')
+
 def main(filepath):
     content_string = get_book_text(filepath)
-    count_words(content_string)
-    count_letters(content_string)
+    letter_dict = count_letters(content_string)
+    dict_list = book_report(letter_dict)
+    print_report(filepath, content_string, dict_list)
 
 main('books/frankenstein.txt')
--- stats.py
+++ stats.py
@@ -1,6 +1,6 @@
 def count_words(book_string):
     word_count = len(book_string.split())
-    print(f'{word_count} words found in the document')
+    print(f'Found {word_count} total words')
 
 def count_letters(book_string):
     letter_dict = {}
@@ -9,4 +9,19 @@ def count_letters(book_string):
         if letter in letter_dict.keys():
             letter_dict[letter] += 1
         else: letter_dict[letter] = 1
-    print(letter_dict)
+    return letter_dict
+
+def sort_on(items):
+    return items["num"]
+
+def book_report(dict):
+    dict_list = []
+    for letter in dict:
+        new_dict = {}
+        if letter.isalpha():
+            count = dict[letter]
+            new_dict["char"] = letter
+            new_dict["num"] = count
+            dict_list.append(new_dict)
+    dict_list.sort(reverse=True, key=sort_on)
+    return dict_list
